8ef9877a5a5af73dce88dda0e15fa675e4ee041b
AlexTanets <alexandra_tan1916@ukr.net>
2022-12-21 14:06:42 +0200

    refactoring app component on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,94 +1,89 @@
-import { Component } from "react";
-import { nanoid } from 'nanoid'
-import { GlobalStyles } from "./GlobalStyles";
-import { ContactForm } from "./ContactForm/ContactForm";
-import { ContactList } from "./ContactList/ContactList";
-import { Filter } from "./Filter/Filter";
-import { Wrapper } from "./wrapper.styled";
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
+import { GlobalStyles } from './GlobalStyles';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { Wrapper } from './wrapper.styled';
 
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-
-export class App extends Component {
-state = {
-  contacts: [],
-  filter: ''
-}
-  
-  componentDidMount() {
- try {
-   const savedContacts = JSON.parse(localStorage.getItem('contacts'));
-   if(savedContacts){ this.setState({contacts: savedContacts})}
-     
+  useEffect(() => {
+    try {
+      const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+      if (savedContacts) {
+        setContacts(savedContacts);
+      }
     } catch (error) {
       console.log(error.message);
     }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    }
-  }
+  }, []);
 
-  addContact = (contact) => {
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-    if (this.state.contacts.length !== 0) {
+  const addContact = contact => {
+    if (contacts.length !== 0) {
+      const isAlredyContact = contacts.some(item => {
+        return item.name.toLowerCase() === contact.name.toLowerCase();
+      });
 
-       const isAlredyContact = this.state.contacts.some(item => {
-      return item.name.toLowerCase() === contact.name.toLowerCase()
-       })
-      
-    if (isAlredyContact) {
-      alert(`${contact.name} is alrady in contacts`)
-      return;
-    }
-      
-    contact.id = nanoid(5);
-    this.setState(prevState => {
-      return {contacts: [...prevState.contacts, contact]}
-    })
-      
-    } else {
-       contact.id = nanoid(5);
-    this.setState({contacts: [contact]})
+      if (isAlredyContact) {
+        alert(`${contact.name} is alrady in contacts`);
+        return;
       }
-   
-}
 
+      contact.id = nanoid(5);
+      setContacts(prevState => {
+        return [...prevState, contact];
+      });
+    } else {
+      contact.id = nanoid(5);
+      setContacts([contact]);
+    }
+  };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  }
-  
+  const deleteContact = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
+  };
 
-  hendleFilterChange = (e) => {
-    this.setState({filter: e.currentTarget.value})
+  const hendleFilterChange = e => {
+    setFilter(e.currentTarget.value);
+  };
 
-  }
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
 
-  getFilteredContacts = () => {
-    const normalizedFilter = this.state.filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
 
-    return this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
-}
+  return (
+    <>
+      <GlobalStyles />
+      <Wrapper>
+        <h1>Phonebook</h1>
+        <ContactForm onFormSubmit={addContact} />
 
-  
-  render() {
- 
-    return (
-         <>
-           <GlobalStyles />
-           <Wrapper>
-             <h1>Phonebook</h1>
-             <ContactForm onFormSubmit={this.addContact} />
-
-          <h2>{this.state.contacts.length === 0 ? 'Here will be your contacts. Add contacts': 'Contacts' }</h2>
-          {this.state.contacts.length > 1 && <Filter value={this.state.filter} onInputChange={this.hendleFilterChange } /> } 
-          {this.state.contacts.length !== 0 && <ContactList contacts={this.getFilteredContacts()} deleteContact={this.deleteContact} /> }
-           </Wrapper>
-         </>
-    )
- 
-};
+        <h2>
+          {contacts.length === 0
+            ? 'Here will be your contacts. Add contacts'
+            : 'Contacts'}
+        </h2>
+        {contacts.length > 1 && (
+          <Filter value={filter} onInputChange={hendleFilterChange} />
+        )}
+        {contacts.length !== 0 && (
+          <ContactList
+            contacts={getFilteredContacts()}
+            deleteContact={deleteContact}
+          />
+        )}
+      </Wrapper>
+    </>
+  );
 };
